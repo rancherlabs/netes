edee1ce9a999bdc515ef871403839a8d4fa538f9
Josh Curl <josh@curl.me>
2017-08-20 20:00:28 -0700

    Use a git mirror of goautoneg

--- vendor.sh
+++ vendor.sh
@@ -9,5 +9,3 @@ rm -rf vendor/github.com/rancher/go-rancher
 git checkout vendor/github.com/rancher/go-rancher
 rm -rf vendor/k8s.io/apiserver
 git checkout vendor/k8s.io/apiserver
-rm -rf vendor/bitbucket.org/ww/goautoneg
-git checkout vendor/bitbucket.org/ww/goautoneg
